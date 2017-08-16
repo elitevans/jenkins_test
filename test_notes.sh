03ce335e2df8e26f365b9dca925870dadd2a7056
elitevans <elitevans@github.com>
2017-08-16 09:20:10 -0400

    some prints

--- test_notes.sh
+++ test_notes.sh
@@ -2,7 +2,9 @@
 
 CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 git_log=/tmp/git_log
-git log > $git_log
+echo "++++++++++++++++++++++"
+echo git log > $git_log
+echo "======================"
 
 if [ -s $git_log ]; then
   $CUR_DIR/test_notes.py $git_log
