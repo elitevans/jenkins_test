1e078c684ef41f46d1b6b25c2c1dfec6ea66bd76
elitevans <elitevans@github.com>
2017-08-16 09:23:09 -0400

    pipe instead of >

--- test_notes.sh
+++ test_notes.sh
@@ -3,7 +3,7 @@
 CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 git_log=/tmp/git_log
 echo "++++++++++++++++++++++"
-echo git log > $git_log
+echo git log | $git_log
 echo "======================"
 
 if [ -s $git_log ]; then
